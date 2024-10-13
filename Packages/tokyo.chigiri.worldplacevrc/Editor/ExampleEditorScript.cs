4274d6b3a4f9140da254c02d13ecc2b374a66857
chigirits <chigirits@gmail.com>
2024-10-13 19:37:34 +0900

    Rename example package to tokyo.chigiri.worldplacevrc

---
+++ Packages/tokyo.chigiri.worldplacevrc/Editor/ExampleEditorScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace tokyo.chigiri.worldplacevrc.editor
+{
+
+    public class ExampleEditorScript
+    {
+        [MenuItem("Tools/WorldPlace.VRC/Test")]
+        static void Test()
+        {
+            EditorUtility.DisplayDialog("Example Script", "Opened This Dialog", "OK");
+        }
+    }
+
+}
