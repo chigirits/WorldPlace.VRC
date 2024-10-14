66d5fafab79c9328e1f676d23fe6330d573eb854
chigirits <chigirits@gmail.com>
2024-10-14 21:40:13 +0900

    Initial implementation

---
+++ Packages/tokyo.chigiri.worldplacevrc/Editor/WorldPlaceEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace tokyo.chigiri.worldplacevrc.editor
+{
+
+    public class WorldPlaceEditor
+    {
+
+        const string FEATURE_NAME = "WorldPlace";
+        const string WORLDPLACE_PREFAB_GUID = "546643312472edf429565a17bdb578ec";
+
+        [MenuItem("GameObject/PasocomMate/WorldPlace", priority=21)]
+        static void CreateWorldPlace()
+        {
+            var path = AssetDatabase.GUIDToAssetPath(WORLDPLACE_PREFAB_GUID);
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            var go = GameObject.Instantiate(prefab);
+            go.name = FEATURE_NAME;
+            go.transform.SetParent(Selection.activeGameObject.transform, false);
+            Undo.RegisterCreatedObjectUndo(go, $"Create {FEATURE_NAME}");
+            Selection.activeGameObject = go;
+        }
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WorldPlace.VRC
 
 WORK IN PROGRESS
+
+# Included Assets
+
+This project contains the following assets:
+
+- [Google Material Symbols](https://fonts.google.com/icons) under Apache License Version 2.0
+- [Ameko](https://bbbbbhimiko.booth.pm/items/5407175) under CC0 license (for development use, not included in VPM)
+
+# License
+
+MIT License
+
+© 2024 Chigiri Tsutsumi 
